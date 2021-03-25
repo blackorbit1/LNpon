b91b63ffaf70ba3cc765c689ac138311046adf5b
Equinox4 <equinox4@users.noreply.github.com>
2021-03-25 14:27:57 +0100

    fixed psql scripts

--- post/postgresql/init.sh
+++ post/postgresql/init.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/bin/env bash
 set -e
 
 useradd www-data
 su www-data
 
 psql -v ON_ERROR_STOP=1 <<-EOSQL
+    DROP DATABASE IF EXISTS posts_db;
+    DROP TABLE IF EXISTS posts;
+    DROP ROLE IF EXISTS "www-data";
+
+    CREATE ROLE "www-data" WITH LOGIN PASSWORD 'www-data';
+    CREATE DATABASE "posts_db" OWNER "www-data";
+
+    \c posts_db
+
     DROP TABLE IF EXISTS posts;
     CREATE TABLE posts (
         id SERIAL PRIMARY KEY,
@@ -14,9 +23,6 @@ psql -v ON_ERROR_STOP=1 <<-EOSQL
         id_user int,
         likes int default 0, -- là on a dit qu'on ferait pas comme ça parce que sinon tout le monde pourrait mettre autant de likes qu'il veut
         deleted boolean default false,
-
-        --PRIMARY KEY (id)
     );
-
 
 EOSQL
--- user/postgresql/init.sh
+++ user/postgresql/init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 set -e
 
 useradd www-data
