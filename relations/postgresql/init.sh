9e04e38a1b34f0b2664920da1227717340fdea06
Equinox4 <equinox4@users.noreply.github.com>
2021-03-30 15:22:01 +0200

    updated relations module

--- relations/postgresql/init.sh
+++ relations/postgresql/init.sh
@@ -1,25 +1,22 @@
 #!/bin/env bash
 set -e
 
+useradd www-data
 su www-data
 
 psql -v ON_ERROR_STOP=1 <<-EOSQL
-    DROP DATABASE IF EXISTS users_db;
-    DROP TABLE IF EXISTS users;
+    DROP DATABASE IF EXISTS relations_db;
+    DROP TABLE IF EXISTS relations;
     DROP ROLE IF EXISTS "www-data";
 
     CREATE ROLE "www-data" WITH LOGIN PASSWORD 'www-data';
     CREATE DATABASE "users_db" OWNER "www-data";
 
-    \c relations_db
+    \c users_db
 
     -- verifier que la table appartient bien à www-data (inherit from database ow)
-    CREATE TABLE users (
+    CREATE TABLE relations (
         id       SERIAL      PRIMARY KEY,
-        pseudo   varchar(50) NOT NULL UNIQUE,
-        status   int         NOT NULL,
-        email    text        NOT NULL UNIQUE,
-        password text        NOT NULL
     );
 
 EOSQL
