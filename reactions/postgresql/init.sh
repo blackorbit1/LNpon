97cf6d89ddbeb3200c4e966d3329ac3ed94ee9a6
Equinox4 <equinox4@users.noreply.github.com>
2021-03-30 15:43:20 +0200

    updated reactions module
    Co-authored-by: blackorbit1 <enzo.dutra@gmail.com>

---
+++ reactions/postgresql/init.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+set -e
+
+su www-data
+
+psql -v ON_ERROR_STOP=1 <<-EOSQL
+    DROP DATABASE IF EXISTS reactions_db;
+    DROP TABLE IF EXISTS reactions;
+    DROP ROLE IF EXISTS "www-data";
+
+    CREATE ROLE "www-data" WITH LOGIN PASSWORD 'www-data';
+    CREATE DATABASE "reactions" OWNER "www-data";
+
+    \c reactions_db
+
+    -- verifier que la table appartient bien à www-data (inherit from database ow)    
+    CREATE TABLE reactions (
+        id bigint,
+        emoji varchar(10) UNIQUE,
+        id_user int,
+        id_post int,
+        deleted boolean default false,
+
+        PRIMARY KEY (id)
+    );
+
+
+
+EOSQL
