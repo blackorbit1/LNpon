66dc067a90819a37e88da90e9077df29448fd8cc
Equinox4 <equinox4@users.noreply.github.com>
2021-04-04 15:21:41 +0200

    updated start and stop scripts

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker-compose -f auth/docker-compose.yml up &> services.log &
-docker-compose -f groupes/docker-compose.yml up &> services.log &
-docker-compose -f images/docker-compose.yml up &> services.log &
-docker-compose -f posts/docker-compose.yml up &> services.log &
-docker-compose -f reactions/docker-compose.yml up &> services.log &
-docker-compose -f relations/docker-compose.yml up &> services.log &
-docker-compose -f users/docker-compose.yml up &> services.log &
-docker-compose -f gateway/docker-compose.yml up &> services.log
+docker-compose -f auth/docker-compose.yml up -d
+docker-compose -f groupes/docker-compose.yml up -d
+docker-compose -f images/docker-compose.yml up -d
+docker-compose -f posts/docker-compose.yml up -d
+docker-compose -f reactions/docker-compose.yml up -d
+docker-compose -f relations/docker-compose.yml up -d
+docker-compose -f users/docker-compose.yml up -d
+docker-compose -f gateway/docker-compose.yml up -d
--- stop.sh
+++ stop.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker-compose -f auth/docker-compose.yml stop &
-docker-compose -f groupes/docker-compose.yml stop &
-docker-compose -f images/docker-compose.yml stop &
-docker-compose -f posts/docker-compose.yml stop &
-docker-compose -f reactions/docker-compose.yml stop &
-docker-compose -f relations/docker-compose.yml stop &
-docker-compose -f users/docker-compose.yml stop &
-docker-compose -f gateway/docker-compose.yml stop &
+docker-compose -f auth/docker-compose.yml stop
+docker-compose -f groupes/docker-compose.yml stop
+docker-compose -f images/docker-compose.yml stop
+docker-compose -f posts/docker-compose.yml stop
+docker-compose -f reactions/docker-compose.yml stop
+docker-compose -f relations/docker-compose.yml stop
+docker-compose -f users/docker-compose.yml stop
+docker-compose -f gateway/docker-compose.yml stop
