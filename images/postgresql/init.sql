13e9878e6dfad71c3c5c603eddb3db95c2a42c05
Equinox4 <equinox4@users.noreply.github.com>
2021-04-08 13:41:40 +0200

    updated images

---
+++ images/postgresql/init.sql
@@ -0,0 +1,10 @@
+ALTER ROLE "www-data" WITH LOGIN PASSWORD 'www-data';
+
+CREATE TABLE images (
+    id SERIAL           PRIMARY KEY,
+    user_id int         NOT NULL,
+    chemin varchar(100) NOT NULL,
+    nature int          NOT NULL
+);
+
+ALTER TABLE auth OWNER TO "www-data";
