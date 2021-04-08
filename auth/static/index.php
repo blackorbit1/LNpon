4f2b00c8ebe18e03942d91944c021fc6b7d7052a
Equinox4 <equinox4@users.noreply.github.com>
2021-04-08 13:44:02 +0200

    updated login action

--- auth/static/index.php
+++ auth/static/index.php
@@ -2,23 +2,23 @@
 
 require_once 'db.php';
 
-
 if (isset($_POST['login'])
     && !empty($_POST['pseudo'])
     && !empty($_POST['password'])) {
 
-    curl("users_nginx:8088/verify_user?pseudo=$_POST['pseudo']&password=$_POST['password']");
+    $res = file_get_contents("users_nginx:8088?action=login&pseudo=$_POST['pseudo']&password=$_POST['password']");
+    $data = json_decode($res);
 
-    // TODO test curl return -> if true then generate token
+    if ($data->{'status'} == 'success') {
+        $stmt = $dbh->prepare('INSERT INTO auth (id_user, token) VALUES(?, ?)');
+        $stmt->execute([$data->{'id_user'}, bin2hex(random_bytes(32))]);
+    }
+
 }
-?>
+else if (isset($_POST['action']
+        && $_POST['action'] == 'logout'
+        && !empty($_POST['pseudo'])) {
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"/>
-    <title>Index</title>
-</head>
-<body>
-</body>
-</html>
+}
+
+?>
