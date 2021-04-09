50f1fff3121916caa0cb41395316da7c3899307e
Equinox4 <equinox4@users.noreply.github.com>
2021-04-10 01:46:52 +0200

    updated users in order to return json data instead of integer

--- users/static/index.php
+++ users/static/index.php
@@ -16,7 +16,7 @@ if (isset($_POST['action'])
     $stmt = $dbh->prepare('INSERT INTO users (pseudo, password, email) VALUES (?, ?, ?)');
     $res = $stmt->execute([$pseudo, $password, $email]);
 
-    echo $res;
+    $output = ['result' => $res ? true : false];
 }
 else if (isset($_POST['action'])
          && $_POST['action'] == 'login'
@@ -26,17 +26,21 @@ else if (isset($_POST['action'])
     $pseudo = $_POST['pseudo'];
     $password = $_POST['password'];
 
-    $stmt = $dbh->prepare('SELECT password FROM users WHERE pseudo = ?');
+    $stmt = $dbh->prepare('SELECT * FROM users WHERE pseudo = ?');
     $res = $stmt->execute([$pseudo]);
 
     if ($res && $stmt->rowCount()) {
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         $password_valid = password_verify($password, $row['password']);
 
-        echo $password_valid ? 1 : 0;
-        exit();
+        $output = ['result' => true, 'id_user' => $row['id_user'], 'pseudo' => $row['pseudo'], 'email' => $row['email']];
     }
-
-    echo 0;
+    else {
+        $output = ['result' => false];
+    }
 }
+
+header('Content-Type: application/json');
+echo json_encode($output);
+
 ?>
