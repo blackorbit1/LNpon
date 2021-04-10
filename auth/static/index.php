ee8e350454772ef98fab96b350482786aa8dae1b
Equinox4 <equinox4@users.noreply.github.com>
2021-04-10 02:16:38 +0200

    implement login and logout actions in auth service

--- auth/static/index.php
+++ auth/static/index.php
@@ -2,23 +2,50 @@
 
 require_once 'db.php';
 
-if (isset($_POST['login'])
+if (isset($_POST['action'])
+    && $_POST['action'] == 'login'
     && !empty($_POST['pseudo'])
     && !empty($_POST['password'])) {
 
-    $res = file_get_contents("users_nginx:8088?action=login&pseudo=$_POST['pseudo']&password=$_POST['password']");
-    $data = json_decode($res);
+    $fields = [
+        'action'    => 'login',
+        'pseudo'    => $_POST['pseudo'],
+        'password'  => $_POST['password']
+    ];
 
-    if ($data->{'status'} == 'success') {
+    $curl = curl_init('users_nginx:8088');
+
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($fields));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+
+    $data = json_decode($result);
+
+    if ($data->{'status'} === true) {
+        $token = bin2hex(random_bytes(32));
         $stmt = $dbh->prepare('INSERT INTO auth (id_user, token) VALUES(?, ?)');
-        $stmt->execute([$data->{'id_user'}, bin2hex(random_bytes(32))]);
-    }
+        $stmt->execute([$data->{'id_user'}, $token]);
 
+        $output = ['status' => true, 'id_user' => $data->{'id_user'}, 'pseudo' => $data->{'pseudo'}, 'email' => $data->{'email'}, 'token' => $token];
+    }
+    else {
+        $output = ['status' => false];
+    }
 }
-else if (isset($_POST['action']
+else if (isset($_POST['action'])
         && $_POST['action'] == 'logout'
         && !empty($_POST['pseudo'])) {
 
+        $stmt = $dbh->prepare('DELETE FROM auth WHERE pseudo = ?');
+        $stmt->execute([$_POST['pseudo']]);
+
+    $output = ['status' => true];
 }
+
+header('Content-Type: application/json');
+echo json_encode($output);
 
 ?>
