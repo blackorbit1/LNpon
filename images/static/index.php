f454820cca0c62a20265037c2f9ca24a47822eb1
Equinox4 <equinox4@users.noreply.github.com>
2021-04-15 15:03:33 +0200

    implement images getters

--- images/static/index.php
+++ images/static/index.php
@@ -1,23 +1,5 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'image_db';
-$username = 'postgres';
-$password = 'postgres';
- 
-$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
-
-$dbh = null;
-try {
-    $dbh = new PDO($dsn);
-    if($dbh) {
-        echo "Connecté à $dbname avec succès!";
-    }
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-
-$res = null;
 if (isset($_POST['create']) // TODO : il faut aussi surement gerer la recupération d'un fichier
     && isset($_POST['user_id'])
     && isset($_POST['chemin'])
@@ -28,55 +10,34 @@ if (isset($_POST['create']) // TODO : il faut aussi surement gerer la recupérat
     $stmt->bindParam(2, $_POST['chemin'], PDO::PARAM_STR, 100);
     $stmt->bindParam(3, $_POST['nature'], PDO::PARAM_INT);
     $res = $stmt->execute();
-} else if(isset($_POST['get']) // image by id
-    && isset($_POST['id'])) {
+}
+else if (isset($_POST['action'])
+        && $_POST['action'] === 'get_image'
+        && !empty($_POST['image_id'])) {
 
-    $stmt = $dbh->prepare('SELECT * FROM images WHERE id = ?');
+    $stmt = $dbh->prepare('SELECT * FROM images WHERE image_id = ?');
 
-    if($stmt->execute()){ 
-        $result = array();
-        $tmp = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
-        
-        $result['user_id'] = $tmp['user_id'];
-        $result['chemin'] = $tmp['chemin'];
-        $result['nature'] = $tmp['nature'];
-
-        $res = json_encode($result);
-    } else {
-        $res = "ERREUR SQL";
+    if ($stmt->execute($_POST['image_id'])) {
+        $output = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
-} else if(isset($_POST['get']) // images by user
-&& isset($_POST['user_id'])) {
+    else {
+        $output = ['status' => false];
+    }
+}
+else if (isset($_POST['action'])
+        && $_POST['action'] === 'get_image'
+        && !empty($_POST['user_id'])) {
 
     $stmt = $dbh->prepare('SELECT * FROM images WHERE user_id = ?');
 
-    if($stmt->execute()){ 
-        $result = array();
-        $tmp = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
-        
-        $result['user_id'] = $tmp['user_id'];
-        $result['chemin'] = $tmp['chemin'];
-        $result['nature'] = $tmp['nature'];
-
-        $res = json_encode($result);
-    } else {
-        $res = "ERREUR SQL";
+    if ($stmt->execute($_POST['user_id'])) {
+        $output = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
+    else {
+        $output = ['status' => false];
+    }
 }
+
+header('Content-Type: application/json');
+echo json_encode($output);
 ?>
-
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"/>
-    <title>Index</title>
-</head>
-<body>
-    <? $res ?>
-</body>
-</html>
