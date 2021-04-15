e45fa5222b7b264e4eb7a0c4f22eef8bb72c442d
Equinox4 <equinox4@users.noreply.github.com>
2021-04-15 15:03:03 +0200

    renommage

--- images/postgresql/init.sql
+++ images/postgresql/init.sql
@@ -1,7 +1,7 @@
 ALTER ROLE "www-data" WITH LOGIN PASSWORD 'www-data';
 
 CREATE TABLE images (
-    id SERIAL           PRIMARY KEY,
+    image_id SERIAL           PRIMARY KEY,
     user_id int         NOT NULL,
     chemin varchar(100) NOT NULL,
     nature int          NOT NULL
