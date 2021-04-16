182aa68a5a7c9f5dfc7608e5dfb164b86563c9c1
Equinox4 <equinox4@users.noreply.github.com>
2021-04-16 10:49:17 +0200

    fixed auth service

--- auth/postgresql/init.sql
+++ auth/postgresql/init.sql
@@ -3,7 +3,7 @@ ALTER ROLE "www-data" WITH LOGIN PASSWORD 'www-data';
 CREATE TABLE auth (
     id              SERIAL PRIMARY KEY,
     id_user         int    NOT NULL,
-    token           int    NOT NULL UNIQUE,
+    token           text    NOT NULL UNIQUE,
     date_expiration date   NOT NULL DEFAULT now() + '1 hour'::interval
 );
 
--- auth/static/index.php
+++ auth/static/index.php
@@ -4,15 +4,16 @@ require_once 'db.php';
 
 if (isset($_POST['action'])
     && $_POST['action'] == 'login'
-    && !empty($_POST['pseudo'])
+    && !empty($_POST['id_user'])
     && !empty($_POST['password'])) {
-
+/*
     $fields = [
         'action'    => 'login',
         'pseudo'    => $_POST['pseudo'],
         'password'  => $_POST['password']
     ];
-
+*/
+/*
     $curl = curl_init('users_nginx:8088');
 
     curl_setopt($curl, CURLOPT_POST, true);
@@ -21,26 +22,21 @@ if (isset($_POST['action'])
 
     $result = curl_exec($curl);
     curl_close($curl);
+*/
+//    $data = json_decode($result);
 
-    $data = json_decode($result);
-
-    if ($data->{'status'} === true) {
         $token = bin2hex(random_bytes(32));
         $stmt = $dbh->prepare('INSERT INTO auth (id_user, token) VALUES(?, ?)');
-        $stmt->execute([$data->{'id_user'}, $token]);
+        $stmt->execute([$_POST['id_user'], "$token"]);
 
-        $output = ['status' => true, 'id_user' => $data->{'id_user'}, 'pseudo' => $data->{'pseudo'}, 'email' => $data->{'email'}, 'token' => $token];
-    }
-    else {
-        $output = ['status' => false];
-    }
+        $output = ['status' => true, 'id_user' => $_POST['id_user'], 'token' => $token];
 }
 else if (isset($_POST['action'])
         && $_POST['action'] == 'logout'
-        && !empty($_POST['pseudo'])) {
+        && !empty($_POST['id_user'])) {
 
-        $stmt = $dbh->prepare('DELETE FROM auth WHERE pseudo = ?');
-        $stmt->execute([$_POST['pseudo']]);
+        $stmt = $dbh->prepare('DELETE FROM auth WHERE id_user = ?');
+        $stmt->execute([$_POST['id_user']]);
 
     $output = ['status' => true];
 }
